65fd5b0a13928e4235ea32bfb405e8e132f72a7f
slavamakhnev <slavinmaxnew@gmail.com>
2025-03-04 17:14:24 +0100

    add info to healthcheck

--- test/views.py
+++ test/views.py
@@ -1,8 +1,12 @@
+import datetime
+
 from django.http import JsonResponse
 from django.views import View
 
+startDate = datetime.datetime.now()
+version = "0.1.1"
 
 class HealthCheckView(View):
     def get(self, request, *args, **kwargs):
         # Add any additional checks here (e.g., database connectivity)
-        return JsonResponse({"status": "ok"}, status=200)
+        return JsonResponse({"status": "ok", "start-time": startDate, "version": version}, status=200)
