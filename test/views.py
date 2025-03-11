e3cc964b86d6533c4f45adfb88094c9b42415479
slavamakhnev <slavinmaxnew@gmail.com>
2025-03-11 17:11:55 +0100

    bump up version

--- test/views.py
+++ test/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 from django.views import View
 
 startDate = datetime.datetime.now()
-version = "0.1.1"
+version = "0.1.2"
 
 class HealthCheckView(View):
     def get(self, request, *args, **kwargs):
