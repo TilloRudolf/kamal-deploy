c56df99bf7a3b64369c9909cbe91f9e858cc2b66
slavamakhnev <slavinmaxnew@gmail.com>
2025-03-11 18:08:05 +0100

    add web-2 run

--- test/views.py
+++ test/views.py
@@ -1,12 +1,14 @@
 import datetime
+import uuid
 
 from django.http import JsonResponse
 from django.views import View
 
 startDate = datetime.datetime.now()
 version = "0.1.2"
+marker = uuid.uuid4()
 
 class HealthCheckView(View):
     def get(self, request, *args, **kwargs):
         # Add any additional checks here (e.g., database connectivity)
-        return JsonResponse({"status": "ok", "start-time": startDate, "version": version}, status=200)
+        return JsonResponse({"status": "ok", "start-time": startDate, "version": version, "marker": marker}, status=200)
