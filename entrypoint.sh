da267bb3d09902f45b88ac2dac39a5694c5baa28
slavamakhnev <slavinmaxnew@gmail.com>
2025-03-19 16:36:34 +0100

    change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 if [ "$RUN_ENTRYPOINT" = "true" ]; then
     echo "Executing entrypoint script..."
     # Main process
-#    exec "$@"
     exec "python manage.py migrate"
+    exec "$@"
 else
     echo "Skipping entrypoint execution."
 #    exec "$@"  # Start container normally
