c66f70f57b0d99debd3c22e00d49a6b6438a63b4
slavamakhnev <slavinmaxnew@gmail.com>
2025-03-19 16:16:00 +0100

    entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ "$RUN_ENTRYPOINT" = "true" ]; then
+    echo "Executing entrypoint script..."
+    # Main process
+    exec "$@"
+else
+    echo "Skipping entrypoint execution."
+    exec "$@"  # Start container normally
+fi
