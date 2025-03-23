dc9f599c19067bad6d24efb8b9c5870b938829e9
slavamakhnev <slavinmaxnew@gmail.com>
2025-03-23 14:03:51 +0100

    show commit hash

--- test/views.py
+++ test/views.py
@@ -1,14 +1,16 @@
 import datetime
+import os
 import uuid
 
 from django.http import JsonResponse
 from django.views import View
 
 startDate = datetime.datetime.now()
-version = "0.1.2"
+# version = "0.1.2"
 marker = uuid.uuid4()
 
 class HealthCheckView(View):
     def get(self, request, *args, **kwargs):
         # Add any additional checks here (e.g., database connectivity)
+        version = os.getenv("KAMAL_VERSION")
         return JsonResponse({"status": "ok", "start-time": startDate, "version": version, "marker": marker}, status=200)
